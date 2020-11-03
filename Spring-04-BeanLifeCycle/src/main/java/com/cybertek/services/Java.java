52596e0936481d4ff36a2606dc89c867288e9944
willoflove@gmail.com <willoflove@gmail.com>
2020-11-03 10:31:24 +0100

    Bean LifeCycle

--- Spring-04-BeanLifeCycle/src/main/java/com/cybertek/services/Java.java
+++ Spring-04-BeanLifeCycle/src/main/java/com/cybertek/services/Java.java
@@ -7,4 +7,12 @@ public class Java implements Course {
     public void getTeachingHours() {
         System.out.println("Weekly teaching hours 20");
     }
+
+    public void myInitMethod(){
+        System.out.println("Executing init method");
+    }
+
+    public void myDestroyMethod(){
+        System.out.println("Executing destroy method");
+    }
 }
