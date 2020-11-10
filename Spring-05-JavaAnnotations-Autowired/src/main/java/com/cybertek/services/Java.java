2f4822e6e9efcbea0b24066bbc2976ec70bb223c
willoflove@gmail.com <willoflove@gmail.com>
2020-11-10 12:45:28 +0100

    Java Annotations - Autowired

--- Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
@@ -1,16 +1,40 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
 
+    //Field Injection
+    @Autowired
     private OfficeHours officeHours;
+/*
+    //Constructor Injection
+    @Autowired
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
+ */
+
+    public OfficeHours getOfficeHours() {
+        return officeHours;
+    }
+/*
+    //Setter Injection
+    @Autowired
+    public void setOfficeHours(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
+ */
 
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours 30" + officeHours.getHours());
+        System.out.println("Weekly teaching hours : " + (30 + officeHours.getHours()));
     }
 
 }
