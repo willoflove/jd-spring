866d7962b499d00eecc5f36dd34ed4b838b617ea
willoflove@gmail.com <willoflove@gmail.com>
2020-11-10 20:59:23 +0100

    Java Code - Configuration

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
@@ -1,14 +1,21 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class Java implements Course {
+
+    private ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
 
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours : 30");
+        System.out.println("Weekly teaching hours : " + (30 + extraSessions.getHours()));
     }
 
 }
